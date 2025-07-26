c43b7176538549b3d5170186212c2776a30adee4
mobifom@gmail.com <mobifom@gmail.com>
2025-07-27 10:55:45 +1200

    Initial Commit

---
+++ EnergyMonitorTests/EnergyMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyMonitorTests.swift
+//  EnergyMonitorTests
+//
+//  Created by Mohamed Hamdi on 27/07/2025.
+//
+
+import Testing
+@testable import EnergyMonitor
+
+struct EnergyMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
